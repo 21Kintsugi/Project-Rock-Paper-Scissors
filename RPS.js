aed806106fb030ab971ac9c2d6356aff3e78eb98
21Kintsugi <cristian21balacci.@gmail.com>
2024-03-11 21:39:22 +0200

    for now code looks like this, but it does not work, it alyas outputs the return from the last else

--- RPS.js
+++ RPS.js
@@ -1,8 +1,46 @@
-function getComputerChoice () {
-    var moves = ["Rock", "Papper", "Scissors"];
+function getComputerChoice() {
+    var moves = ["Rock", "Paper", "Scissors"];
     var randomIndex = Math.floor(Math.random() * moves.length);
     return moves[randomIndex];
 }
 
 var randomMove = getComputerChoice();
-console.log(randomMove);
+/* console.log("Computer's move:", randomMove); */
+
+function singleRound (playerSelection, computerSelection) {
+
+    console.log("Player Selection:", playerSelection);
+    console.log("Computer Selection:", randomMove);
+
+   if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Paper") {
+    return "You Win! Scissors beats Paper";
+   } 
+   else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Rock") {
+    return "You Lose! Rock beats Scissors";
+   } 
+   else if (playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Scissors") {
+    return"It's a tie!";
+   } 
+   else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === "Paper") {
+    return"You Lose! Paper beats Rock";
+   } 
+   else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === "Rock") {
+    return "It's a tie!";
+   } 
+   else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === "Scissors") {
+    return "You Win! Rock beats Scissors";
+   } 
+   else if (playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === "Paper") {
+    return "It's a tie!";
+   } 
+   else if (playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === "Rock") {
+    return "You Win! Paper beats Rock";
+   } 
+   else {
+    return "You Lose! Scissors beat Paper";
+   }
+}
+
+var result = singleRound("Paper", randomMove);
+
+console.log(result);
