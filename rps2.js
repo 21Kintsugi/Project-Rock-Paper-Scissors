02cbe4554b85f170b2b24f82e2aa71e3c9761534
21Kintsugi <cristian21balacci.@gmail.com>
2024-03-14 21:41:09 +0200

    added an input element to the html

--- rps2.js
+++ rps2.js
@@ -12,11 +12,11 @@ function singleRound (playerSelection, computerSelection) {
     console.log("Computer selected", computerSelection.toUpperCase());
     
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return "It's a tie! " + playerSelection.toUpperCase() + " ties with " + computerSelection.toUpperCase();
     } else if (playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win!", playerSelection.toUpperCase(), "beats", computerSelection.toUpperCase();
+        return "You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();
     } else {
-        return "You Lose", computerSelection.toUpperCase(), "beats", playerSelection.toUpperCase();
+        return "You Lose! " + computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase();
     }
 
 }
